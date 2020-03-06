9010378d08cc5a456aa273562cdf5a69d73a3bcd
GirlsWhoCodeTD <girlswhocodetd@gmail.com>
2020-03-06 22:37:07 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var name = "Hi, my name is Audra";
+document.getElementById("dynamicText").innerHTML = name;
+
+function showMessage(){
+  document.getElementById("secret").innerHTML = "You can do it!";
+}
